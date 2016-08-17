b06841aa1215a817a9e8953b39db752d09143f85
archfiery <archfiery@gmail.com>
2016-08-17 23:40:58 +1000

    initial attempt add hash-object, which creates a blob from file, and save the object to odb add pack-objects that packs objects as pack and idx files.

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <git2.h>
+
+void print_usage(void);
+
+#endif
--- git2.c
+++ git2.c
@@ -1,5 +1,25 @@
+#include "builtin.h"
+#include "common.h"
+
 int main (int argc, char const *argv[])
 {
 	/* code */
+	int i;
+	if (argc < 2) {
+		print_usage();
+	}
+
+	char command[128];
+	strcpy(command, argv[1]);
+
+	if (strcmp(command, "hash-object") == 0) {
+		cmd_hash_object(argc, argv, "");
+	} else if (strcmp(command, "pack-objects") == 0) {
+		cmd_pack_objects(argc, argv, "");
+	} else {
+		fprintf(stderr, "Unknown command %s.\n", command);
+		print_usage();
+	}
+
 	return 0;
 }
